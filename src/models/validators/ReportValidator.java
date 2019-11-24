b6d4314543f990672206c5c35ae44ba096cee0ff
kento.yamamoto <dragonkid828828k@gmail.com>
2019-11-25 00:15:21 +0900

    Add Report class

---
+++ src/models/validators/ReportValidator.java
@@ -0,0 +1,41 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Report;
+
+public class ReportValidator {
+    public static List<String> validate(Report r) {
+        List<String> errors = new ArrayList<String>();
+//必須入力のチェック
+        String title_error = _validateTitle(r.getTitle());
+        if(!title_error.equals("")) {
+            errors.add(title_error);
+        }
+
+        String content_error = _validateContent(r.getContent());
+        if(!content_error.equals("")) {
+            errors.add(content_error);
+        }
+
+        return errors;
+    }
+//空の場合表示される
+    private static String _validateTitle(String title) {
+        if(title == null || title.equals("")) {
+            return "タイトルを入力してください。";
+            }
+
+        return "";
+    }
+
+    private static String _validateContent(String content) {
+        if(content == null || content.equals("")) {
+            return "内容を入力してください。";
+            }
+
+        return "";
+    }
+
+}
